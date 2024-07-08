802cdac155431e40041ec31fc30952478b7a52a9
Jisbel C. Rodriguez <jisbelcarolina2906@gmail.com>
2024-07-08 09:02:52 -0400

    project style settings

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    < >
+    <div className='font-karla400'>
+
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -27,6 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+    </div>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+
+        // Primary
+
+        Green200lighter: 'hsl(148, 38%, 91%)',
+        Green600medium:' hsl(169, 82%, 27%)',
+        Red: 'hsl(0, 66%, 54%)',
+        
+        // Neutral
+
+        Grey500medium: 'hsl(186, 15%, 59%)',
+        Grey900darker: 'hsl(187, 24%, 22%)',
+      },
+      fontFamily: {
+        karla400: ['Karla400', 'serif'],
+        karla700: ['Karla700', 'serif'],
+      
+      },
+    },
   },
   plugins: [],
 }
